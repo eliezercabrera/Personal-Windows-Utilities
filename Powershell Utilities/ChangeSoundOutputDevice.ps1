5beb341380f4f9b25b2fd2409bf2740e9ea7d01c
Eliezer Cabrera <eliezercabrera@gmail.com>
2024-10-16 22:08:20 -0700

    Update headphones, improve powershell script that switches sound output device by declaring variables for headphones and speaker.

--- Powershell Utilities/ChangeSoundOutputDevice.ps1
+++ Powershell Utilities/ChangeSoundOutputDevice.ps1
@@ -1,1 +1,6 @@
-SoundVolumeView /SwitchDefault "{0.0.0.00000000}.{c2cebce3-a832-45a6-b6b0-24bac4cbff70}" "{0.0.0.00000000}.{8df34c78-23d5-4495-8290-cc018db15f6c}"
+# Get ids by running SoundVolumeView
+
+$speakers = "{0.0.0.00000000}.{c2cebce3-a832-45a6-b6b0-24bac4cbff70}"
+$headphones = "{0.0.0.00000000}.{05be4fed-c83e-4004-be83-b7085d97a0ef}"
+
+SoundVolumeView /SwitchDefault $headphones $speakers
