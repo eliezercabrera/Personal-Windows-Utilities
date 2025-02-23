099d8a8fecedb9bfda060b0acd7fd737420b3b59
Eliezer Cabrera <eliezercabrera@gmail.com>
2025-02-23 15:41:15 -0800

    Adds Private Internet Access winget command.

--- Powershell Utilities/ChangeSoundOutputDevice.ps1
+++ Powershell Utilities/ChangeSoundOutputDevice.ps1
@@ -1,6 +1,6 @@
 # Get ids by running SoundVolumeView
 
-$speakers = "{0.0.0.00000000}.{c2cebce3-a832-45a6-b6b0-24bac4cbff70}"
-$headphones = "{0.0.0.00000000}.{05be4fed-c83e-4004-be83-b7085d97a0ef}"
+$speakers = "{0.0.0.00000000}.{25e14338-ff42-464b-981e-5f8e8adbf1b7}"
+$headphones = "{0.0.0.00000000}.{0e77df00-972a-45ae-988f-0763f02b72af}"
 
 SoundVolumeView /SwitchDefault $headphones $speakers
