e3f8f2d0423590216b43c5c561e25ccb9c4bcbde
Eliezer Cabrera <eliezercabrera@gmail.com>
2025-06-08 12:27:36 -0700

    Use names instead of ids for sound output devices in script that switches output device from headphones to speakers. Names are more resilient to hardware changes, which might include just unplugging and replugging things.

--- Powershell Utilities/ChangeSoundOutputDevice.ps1
+++ Powershell Utilities/ChangeSoundOutputDevice.ps1
@@ -1,6 +1,6 @@
 # Get ids by running SoundVolumeView
 
-$speakers = "{0.0.0.00000000}.{25e14338-ff42-464b-981e-5f8e8adbf1b7}"
-$headphones = "{0.0.0.00000000}.{0e77df00-972a-45ae-988f-0763f02b72af}"
+$speakers = "Schiit - Yamaha S8 Speakers"
+$headphones = "JDS Labs - Sennheisser HD800s Headphones"
 
 SoundVolumeView /SwitchDefault $headphones $speakers
